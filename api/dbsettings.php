c2fe7d6106dc3db06c838602aa11cbe48a48b1cf
Arctic Code <tehfoxy.c0de@gmail.com>
2014-03-27 16:30:41 -0500

    Revert "Move APIKey to different file"
    This reverts commit c1c557ed07f179ef48ce3d257b78aabe67b63ad6.
    
    Conflicts:
    	api/dbsettings.php

--- api/dbsettings.php
+++ api/dbsettings.php
@@ -2,12 +2,10 @@
 
 // DBSettings
 
-global $apidb = new mysqli('localhost', 'api', 'password', 'api'); // Connect to main APIDB
+$apidb = new mysqli('localhost', 'api', 'password', 'api'); // Connect to main APIDB
 if($apidb->connect_errno > 0) die('Unable to connect to database [' . $apidb->connect_error . '] - Check dbsettings.php');
 
-global $shortdb = new mysqli('localhost', 'short', 'password', 'short'); // Connect to link shortener DB
+$shortdb = new mysqli('localhost', 'short', 'password', 'short'); // Connect to link shortener DB
 if($shortdb->connect_errno > 0) die('Unable to connect to database [' . $shortdb->connect_error . '] - Check dbsettings.php');
-
-global $key = '9a211e90b0a0570ed33e47428231e702af47b6f54fb347960f661184e063a1d0'; // KEEP THIS PRIVATE! This is the only thing that authenticates the application
 
 ?>
